76f7257f7ffdfeb9832224c4fbf46092cae723cf
n-michlol <neriah@hamichlol.org.il>
2025-02-28 12:18:02 +0200

    Notifications fix

---
+++ EditedPresentationModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MediaWiki\Extension\AspaklaryaReview\Notifications;
+
+use EchoEventPresentationModel;
+use Message;
+
+class EditedPresentationModel extends EchoEventPresentationModel {
+    public function getIconType() {
+        return 'edit';
+    }
+
+    public function getHeaderMessage() {
+        $filename = $this->event->getExtraParam('filename');
+        return new Message('aspaklarya-notification-edited', [$filename]);
+    }
+
+    public function getBodyMessage() {
+        return false;
+    }
+
+    public function getPrimaryLink() {
+        $filename = $this->event->getExtraParam('filename');
+        return [
+            'url' => \Title::newFromText($filename, NS_FILE)->getFullURL(),
+            'label' => $this->msg('notification-link-text-view-file')->text()
+        ];
+    }
+}
